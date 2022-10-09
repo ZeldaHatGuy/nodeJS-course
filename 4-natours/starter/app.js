b56f9493eed345b8eda99fb9746904e2cdb3f229
Greg Singletary <gksingl@wal-mart.com>
2022-10-08 19:36:47 -0500

    all of the things

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,18 +1,71 @@
 'use strict';
 const express = require('express');
+const fs = require('fs');
+const { toUSVString } = require('util');
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res
-    .status(200)
-    .json({message: 'Hello from the server side!', app: 'Natours'});
+// app.get('/', (req, res) => {
+//     res
+//     .status(200)
+//     .json({message: 'Hello from the server side!', app: 'Natours'});
+// })
+
+// app.post('/', (req, res) => {
+//     res
+//     .send('You can post here')
+// })
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    });
+});
+
+
+app.get('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id == id);
+
+    if (!tour) {
+        return res.status(404).json({
+            status: "Failed",
+            message: "Invalid ID",
+        })
+    }
+    res.status(200).json({
+        status: 'success',
+         data: {
+             tour
+         }
+    });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body);
+    const newID = tours[tours.length-1].id +1;
+    const newTour = Object.assign({id: newID}, req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
+      }  
+    )
 })
 
-app.post('/', (req, res) => {
-    res
-    .send('You can post here')
-})
+    
+
 const port = 3000
 
 app.listen(port, () => {
