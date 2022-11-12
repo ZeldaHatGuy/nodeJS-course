b976906fd977ae880dd60408b35ed26e1aba6fb1
ZeldaHatGuy <Equin0xx@yahoo.com>
2022-11-11 23:17:59 -0600

    adding db stuff

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,93 +1,84 @@
 const fs = require('fs');
-const convertID = require('./helpers')
+const convertID = require('./helpers');
 
 const tours = JSON.parse(
-   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+//Middleware function
+exports.checkID = (req, res, next, val) => {
+  id = convertID(req.params.id);
+  if (!tours.find((el) => el.id === id)) {
+    return res.status(404).json({
+      status: 'Failure',
+      message: `Unable to locate ${req.params.id}`,
+    });
+  }
+  next();
+};
 
-// validates ID exists in file and returns a boolean
-function validateID(id) {
-      const tour = tours.find(el => el.id === id);
-      if(!tour) {
-        return false 
-      }
-      return true
-}
-
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'Failure',
+      message: 'You sent a bad request',
+    });
+  }
+  next();
+};
+//tour handler functions
 exports.getAllTours = (req, res) => {
-    res.status(200).json({
-        status: "Success",
-        requestTime: req.requestTime,
-        results: tours.length,
-        data: {
-            tours
-        }
-    })
-}
+  res.status(200).json({
+    status: 'Success',
+    requestTime: req.requestTime,
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
 
 exports.getTour = (req, res) => {
-        const id =  convertID(req.params.id)
-    if ( !validateID(id)) {
-        return res.status(404).json({ 
-            message: `no tour with id ${id} has been found`, 
-            status: "Failure"
-        });
-    }
-     const tour = tours.find(el => el.id == id)
-     res.status(200).json({
-        status: 'Success',
-        requestTime: req.requestTime,
-         data: {
-             tour: tour
-         }
-     });
-}
+  const tour = tours.find((el) => el.id == id);
+  res.status(200).json({
+    status: 'Success',
+    requestTime: req.requestTime,
+    data: {
+      tour: tour,
+    },
+  });
+};
 
 exports.createTour = (req, res) => {
-    const newId = tours[tours.length -1].id + 1;
-    const newTour = Object.assign({id: newId}, req.body);
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: "Success",
-            data: {
-                tour: newTour
-            }
-        });
-    });
-}
-
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { id: newId, ...req.body };
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'Success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
 
 exports.updateTour = (req, res) => {
-    const id = convertID(req.params.id)
-    if (!validateID(id)) {
-        return res.status(404).json({
-            status: "Failure",
-            message: `No tour with id ${id} has been found` 
-
-        });
-    }
-    res.status(200).json({
-        status: "Success",
-        data: {
-            tour: "<updated tour>"
-        }
-    });
-}
+  res.status(200).json({
+    status: 'Success',
+    data: {
+      tour: '<updated tour>',
+    },
+  });
+};
 
 exports.deleteTour = (req, res) => {
-    const id = convertID(req.params.id)
-    if (!validateID(id)) {
-        return res.status(404).json({
-            status: "Failure",
-            message: `No tour with id ${id} has been found` 
-
-        });
-    }
-    res.status(204).json({
-        status: "Success",
-        data: null
-        
-    });
-}
+  res.status(204).json({
+    status: 'Success',
+    data: null,
+  });
+};
--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -1,19 +1,20 @@
-const express = require('express')
+const express = require('express');
+
 const router = express.Router();
 const fs = require('fs');
-const tourController = require('./../controllers/tourController')
+const tourController = require('../controllers/tourController');
 
+router.param('id', tourController.checkID);
 
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour)
+  .post(tourController.checkBody, tourController.createTour);
 
 router
   .route('/:id')
   .get(tourController.getTour)
   .patch(tourController.updateTour)
-  .delete(tourController.deleteTour)
-
+  .delete(tourController.deleteTour);
 
 module.exports = router;
